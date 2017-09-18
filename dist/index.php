7cdc86166b3e87189b345c3fe65164444ee35a25
cooky <746996050@qq.com>
2017-09-18 10:50:37 +0800

    仿易企秀效果，整页滑动

---
+++ dist/index.php
@@ -0,0 +1,568 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no" />
+	<meta name="description" itemprop="description" content="">
+	<meta itemprop="name" content="">
+	<meta itemprop="image" content="./images/share.png">
+    <link rel="stylesheet" type="text/css" href="./css/index.css">
+</head>
+<body>
+	<img src="images/share.png" alt="" style="position: absolute;left: -9999px;opacity: 0;">
+	<!-- 音乐 end-->
+	<!--div id="audio" class="music off rotate">
+		<audio src="music.mp3" controls="false" loop>
+			您的浏览器不支持html5，请更换更高级浏览器。
+		</audio>
+		<img class="musicChange" src="images/open.gif"/>
+		<img class="musicQuiet" src="images/music.png"/>
+	</div-->
+	<div id="audio" class="music rotate" data-event="11205" style="display: block;">
+		<audio loop="" src="music.mp3" id="media" autoplay="" preload=""></audio>
+	</div>
+	<!-- 音乐 end-->
+	<!-- 禁止横屏 start-->
+	<section id="lock" class="lock_wrp">
+        <div class="lock">
+            <i></i><br>为了更好的体验，请使用竖屏浏览
+        </div>
+    </section>
+    <!-- 禁止横屏 end-->
+    <!-- 起始动画 top start-->
+    <!--section class="top">
+		<div class="bg-half"></div>
+		<div class="bg-half"></div>
+		<div class="clearfix dial">
+			<div class="dial-half">
+				<img src="images/top/dial-left.png" alt="">
+			</div>
+			<div class="dial-half">
+				<img src="images/top/dial-right.png" alt="">
+			</div>
+			<div class="dial-num">
+				<img src="images/top/dial-num.png" alt="">
+			</div>
+		</div>
+	</section-->
+	<!-- 起始动画 top end-->
+    <section class="wrap">
+    	<!-- 第一屏 start-->
+    	<section class="page page1 on" data-id='1'>
+			<div class="box">
+				<div class="inside1">
+					<div class="element1">
+						<img id="" ctype="" class="" draggable="false" src="./images/page1/element1.jpg" style="">
+					</div>
+				</div>
+				<div class="inside2">
+					<div class="element2">
+						<img id="" ctype="" class="" draggable="false" src="./images/page1/element2.png" style="">
+					</div>
+				</div>
+				<div class="inside3">
+					<div class="element3">
+						<img id="" ctype="" class="" draggable="false" src="./images/page1/element3.png" style="">
+					</div>
+				</div>
+				<div class="inside4">
+					<div class="element4">
+						<div class="editable-text">
+							<span style="">看外面</span>
+							<div>
+								<span style="">那是朕给你打下的江山</span>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="btn-arrow">
+					<img src="images/btn-arrow.png" alt="">
+				</div>
+			</div>
+    	</section>
+    	<!-- 第一屏 end-->
+    	<!-- 第二屏 start-->
+    	<div class="page page2" data-id='2'>
+    		<div class="box">
+    			<div class="bg"></div>
+				<div class="page2-logo">
+					<img src="images/page2/page2-logo.png" alt="">
+				</div>
+				<div class="page2-text page2-text1">
+					<img src="images/page2/page2-text1.png" alt="">
+				</div>
+				<div class="page2-text page2-text2">
+					<img src="images/page2/page2-text2.png" alt="">
+				</div>
+				<div class="page2-text page2-text3">
+					<img src="images/page2/page2-text3.png" alt="">
+				</div>
+				<div class="page2-text page2-text4">
+					<img src="images/page2/page2-text4.png" alt="">
+				</div>
+				<div class="page2-text page2-text5">
+					<img src="images/page2/page2-text5.png" alt="">
+				</div>
+				<div class="page2-text page2-text6">
+					<img src="images/page2/page2-text6.png" alt="">
+				</div>
+				<div class="page2-text page2-text7">
+					<img src="images/page2/page2-text7.png" alt="">
+				</div>
+				<div class="btn-arrow">
+					<img src="images/btn-arrow.png" alt="">
+				</div>
+    		</div>
+    	</div>
+    	<!-- 第二屏 end-->
+    	<!-- 第三屏 start-->
+    	<section class="page page3" data-id='3'>
+			<div class="box">
+				<div class="bg bg-opa"></div>
+				<div class="slogan">
+					<img src="images/page3/page3-slogan.png" alt="">
+				</div>
+				<div class="text">
+					<img src="images/page3/page3-text.png" alt="">
+				</div>
+				<div class="people">
+					<img src="images/page3/page3-people.png" alt="">
+				</div>
+				<div class="btn-arrow">
+					<img src="images/btn-arrow.png" alt="">
+				</div>
+			</div>
+    	</section>
+    	<!-- 第三屏 end-->
+    	<!-- 第四屏 start-->
+    	<section class="page page4" data-id='4'>
+			<div class="box">
+				<div class="bg bg-opa"></div>
+				<div class="slogan">
+					<img src="images/page4/page4-slogan.png" alt="">
+				</div>
+				<div class="text">
+					<img src="images/page4/page4-text.png" alt="">
+				</div>
+				<div class="people">
+					<img src="images/page4/page4-people.png" alt="">
+				</div>
+				<div class="btn-arrow">
+					<img src="images/btn-arrow.png" alt="">
+				</div>
+			</div>
+    	</section>
+    	<!-- 第四屏 end-->
+    	<!-- 第五屏 start-->
+    	<section class="page page5" data-id='5'>
+			<div class="box">
+				<div class="bg bg-opa"></div>
+				<div class="slogan">
+					<img src="images/page5/page5-slogan.png" alt="">
+				</div>
+				<div class="text">
+					<img src="images/page5/page5-text.png" alt="">
+				</div>
+				<div class="people">
+					<img src="images/page5/page5-people.png" alt="">
+				</div>
+				<div class="btn-arrow">
+					<img src="images/btn-arrow.png" alt="">
+				</div>
+			</div>
+    	</section>
+    	<!-- 第五屏 end-->
+    	<!-- 第六屏 start-->
+    	<section class="page page6" data-id='6'>
+			<div class="box">
+				<div class="bg bg-opa"></div>
+				<div class="slogan">
+					<img src="images/page6/page6-slogan.png" alt="">
+				</div>
+				<div class="text">
+					<img src="images/page6/page6-text.png" alt="">
+				</div>
+				<div class="people">
+					<img src="images/page6/page6-people.png" alt="">
+				</div>
+				<div class="btn-arrow">
+					<img src="images/btn-arrow.png" alt="">
+				</div>
+			</div>
+    	</section>
+    	<!-- 第六屏 end-->
+    	<!-- 第七屏 start-->
+    	<section class="page page7" data-id='7'>
+			<div class="box">
+				<div class="bg bg-opa"></div>
+				<div class="slogan">
+					<img src="images/page7/page7-slogan.png" alt="">
+				</div>
+				<div class="text">
+					<img src="images/page7/page7-text.png" alt="">
+				</div>
+				<div class="people">
+					<img src="images/page7/page7-people.png" alt="">
+				</div>
+				<div class="btn-arrow">
+					<img src="images/btn-arrow.png" alt="">
+				</div>
+			</div>
+    	</section>
+    	<!-- 第七屏 end-->
+    	<!-- 第八屏 start-->
+    	<section class="page page8" data-id='8'>
+			<div class="box">
+				<div class="bg bg-opa"></div>
+				<div class="page8-text1">
+					<img src="images/page8/page8-text1.png" alt="">
+				</div>
+				<div class="finger">
+					<img class="finger-pic" src="images/page8/finger.png" alt="">
+					<img class="finger-move" src="images/page8/finger-move.png" alt="">
+				</div>
+				<div class="page8-text2">
+					<img src="images/page8/page8-text2.png" alt="">
+				</div>
+				<div class="page8-text3">
+					<img src="images/page8/page8-text3.png" alt="">
+				</div>
+				<div class="page8-code">
+					<img src="images/page8/page8-code.png" alt="">
+				</div>
+				<div class="page8-text4">
+					<img src="images/page8/page8-text4.png" alt="">
+				</div>
+				<div class="btn-arrow">
+					<img src="images/btn-arrow.png" alt="">
+				</div>
+			</div>
+    	</section>
+    	<!-- 第八屏 end-->
+	</section>
+    <script type="text/javascript" src="js/zepto.min.js"></script>
+    <script type="text/javascript" src="js/imagesloaded.min.js"></script>
+    <script type="text/javascript">
+    	$(function(){
+    		/**
+			 * 常用工具
+			 */
+    		var utils = {
+    			supports:function(prop) { 
+					var div = document.createElement('div'), 
+					vendors = 'Khtml O Moz Webkit'.split(' '), 
+					len = vendors.length; 
+					return function(prop) { 
+						if (prop in div.style) return true; 
+						if ('-ms-' + prop in div.style) return true; 
+						prop = prop.replace(/^[a-z]/, function(val) { 
+							return val.toUpperCase(); 
+						}); 
+						while(len--) { 
+							if (vendors[len]+prop in div.style ) {
+								return true; 
+							}
+						} 
+						return false; 
+					}(prop); 
+				},
+				prefix:function () {
+					var styles = window.getComputedStyle(document.documentElement, ''),
+					pre = (Array.prototype.slice
+					.call(styles)
+					.join('') 
+					.match(/-(moz|webkit|ms)-/) || (styles.OLink === '' && ['', 'o'])
+					)[1],
+					dom = ('WebKit|Moz|MS|O').match(new RegExp('(' + pre + ')', 'i'))[1];
+					return {
+						dom: dom,
+						lowercase: pre,
+						css: '-' + pre + '-',
+						js: pre[0].toUpperCase() + pre.substr(1)
+					};
+				}
+    		}
+
+    		var v_w = $(window).width(); //图片区域最大宽度
+			var v_h = $(window).height(); //图片区域最大高度
+			// 欢迎页高度稳定
+			$(".top").height(v_h);
+
+			// 显示动画
+			$('.top').addClass('play');
+			//定时器首屏展开
+            var topTime = setTimeout(function(){
+            	if (!!$('.top').hasClass('play')) {
+            		$('.dial-num').hide();
+            		$('.top').removeClass('play');
+            		var timer = setTimeout(function(){
+						$('.top').remove();
+					}, 600);
+					init();
+            	};
+            }, 5000);
+
+			
+			// 首屏展开
+			$('.top').on('touchstart', function(){
+				$(this).find('.dial-num').hide();
+				$(this).removeClass('play');
+				var timer = setTimeout(function(){
+					$('.top').remove();
+				}, 600);
+				init();
+			});
+			init();
+
+			// 指纹点击
+			$('.finger').on('tap', function(e){
+				e.preventDefault();
+				$(this).addClass('flipOutX');
+				$('.page8-text1').addClass('flipOutX');
+				$('.page8-text2').addClass('flipInX');
+			});
+
+			var $audio = $("#audio");
+            //音乐自动播放
+        	$audio.find("audio")[0].play();
+			// 音乐切换
+			$audio.on('tap', function(){
+				if ($audio.hasClass('rotate')) {
+					$audio.find("audio")[0].pause();
+					$audio.removeClass('rotate');
+				} else {
+					$audio.find("audio")[0].play();
+					$audio.addClass('rotate');
+				}
+				// if ($('.musicChange').hasClass('hide')) {
+				// 	$("#audio").find("audio")[0].play();
+				// 	$('.musicChange').removeClass('hide');
+				// } else{
+				// 	$('.musicChange').addClass('hide');
+				// 	$("#audio").find("audio")[0].pause();
+				// };
+			});
+
+			/**
+			 * 滑动功能
+			 */
+			
+            function init(){
+            	$('.wrap').height(v_h);
+			    setTimeout(function () {
+			        $('.page1').addClass('play');
+			    	touchSlider.init();
+			    },500);
+            }
+            //控制不横屏
+            var touch=function (e) { e.preventDefault(); };
+		    var l = function() {
+		        var e = document.getElementById("lock");
+		        return {
+		            show: function() {
+		                window.scroll(0, 0), e.style.display = "block"
+						window.addEventListener('touchmove',touch, false);
+		            },
+		            hide: function() {
+		                window.scroll(0, 0), e.style.display = "none";
+						window.removeEventListener('touchmove',touch, false);
+		            }
+		        }
+		    }(),
+		    c = !("onorientationchange" in window);
+		    c || (window.addEventListener("orientationchange", function() {
+		        window.orientation != 0 ? l.show() : l.hide()
+		    }, !1), window.orientation != 0 && l.show());
+			document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
+			
+			//触屏事件
+			var prefix=utils.prefix();
+			var touchSlider = {
+			    init:function () {
+			    	this.isDown=false;
+			        this.touchInitPos = 0;//手指初始位置
+			        this.startPos = 0;//移动开始的位置
+			        this.totalDist = 0,//移动的总距离
+	                this.deltaX1 = 0;//每次移动的正负
+			        this.deltaX2 = 0;//每次移动的正负
+			        this.cur=parseInt($('.on').data('id'));//当前第几个
+			        this.length=$('.wrap .page').size();//总页数
+			
+			        var self = this;
+			        $('body').on({
+			        	'touchstart':function (e) {self.touchstart(e);},
+			        	'touchmove':function (e) {self.touchmove(e);},
+			        	'touchend':function (e) {self.touchend(e);},
+			        	'touchcancel':function (e) {reset();}
+		        	});
+			    },
+			    touchstart : function (e) {
+			    	this.isDown=true;
+			    	var ev=window.event.touches;
+			        if(ev.length !== 1){return;}//如果大于1个手指，则不处理
+			        this.touchInitPos = ev[0].pageY; // 每次move的触点位置
+			        this.deltaX1 = this.touchInitPos;//touchstart的时候的原始位置
+			
+			        this.startPos = 0;
+			        this.startPosPrev = -v_h;
+			        this.startPosNext = v_h;
+			        
+			        this.hasPrev =(this.cur-1)>0;
+			        this.hasNext =(this.cur+1)<=this.length;
+			        //手指滑动的时候禁用掉前后动画
+		            var _cur=$('.on');
+		            _cur.addClass('touchmove');
+		            var _id=parseInt(_cur.data('id'));
+		            $('.page'+(_id-1)+",.page"+(_id+1)).addClass('touchmove');
+			    },
+			    touchmove : function (e) {
+			    	var ev=window.event.touches;
+			        if(ev.length !== 1||!this.isDown){return;}
+
+			        var currentX = ev[0].pageY;
+			        this.deltaX2 = currentX - this.deltaX1;//记录当次移动的偏移量
+			        this.totalDist = this.startPos + currentX - this.touchInitPos;
+			        var pos= '0,'+this.totalDist+'px';
+			        $('.on').css(prefix.css+'Transform','translate3d(' +  pos+ ', 0)');
+			        this.startPos = this.totalDist;
+			        
+			        //处理上一张和下一张
+			        if (this.totalDist<0) {//露出下一张
+			            if (this.hasNext) {
+			                this.totalDist2 = this.startPosNext + currentX - this.touchInitPos;
+			                var pos2= this.totalDist2+'px';
+			                $('.page'+(this.cur+1)).css(prefix.css+'Transform','translate3d(0,'+pos2+',0)');
+			                this.startPosNext = this.totalDist2;
+			            }
+			        }else {//露出上一张
+			            if (this.hasPrev) {
+			                this.totalDist2 = this.startPosPrev + currentX - this.touchInitPos;
+			                var pos2= this.totalDist2+'px';
+			        		$('.page'+(this.cur-1)).css(prefix.css+'Transform','translate3d(0,'+pos2+',0)');
+			                this.startPosPrev = this.totalDist2;
+			            }
+			        }
+			        this.touchInitPos = currentX;
+			    },
+			    touchend : function (e) {
+			    	this.isDown=false;
+		            $('.touchmove').removeClass('touchmove');
+			        if(this.deltaX2<-80){
+			            next();
+			        }else if(this.deltaX2 > 80){
+			            prev();
+			        }else{
+			            this.restore();
+			        }
+			        this.deltaX2 = 0;
+			    },
+			    restore : function () {//touch未遂 复位
+			        reset();
+			    }
+			}
+			//复位
+			function reset() {
+        		var cur=$('.on');
+        		cur.css(prefix.css+'Transform','translate3d(0,0, 0)');
+        		$('.page').each(function(i,n){
+        			var id=parseInt($(this).data('id'));
+        			if(id<touchSlider.cur){//当前节点之前的节点
+        				$(this).css(prefix.css+'Transform','translate3d(0,-'+v_h+'px, 0)');
+        			}else if(id>touchSlider.cur){//当前节点之后的节点
+        				$(this).css(prefix.css+'Transform','translate3d(0,'+v_h+'px, 0)');
+        			}
+        		})
+			}
+			//跳到上一页
+			function prev() {
+			    if (touchSlider.cur-1> 0) {
+			        touchSlider.cur--;
+			    }else {
+			        reset();
+			        return false;
+			    }
+			
+			    var prevIndex = touchSlider.cur+1 > touchSlider.length ? 1 : touchSlider.cur;
+			
+			    var current=$('.on');
+			    var prev=$('.page'+prevIndex);
+			    
+			    current.css(prefix.css+'Transform','translate3d(0,'+v_h+'px,0)')
+			    	.removeClass('on');
+			    prev.css(prefix.css+'Transform','translate3d(0,0,0)')
+			    	.addClass('on');
+			    	
+			    if(utils.supports(prefix.lowercase+'Transition')){
+					current.unbind()
+						.bind('webkitTransitionEnd otransitionend msTransitionEnd transitionend',function(e){
+			            if ($('.play')) {
+			                $('.play').removeClass('play');
+			            }
+			            $('.on').addClass('play');
+			        });
+			    }else{
+		            if ($('.play')) {
+		                $('.play').removeClass('play');
+		            }
+		            $('.on').addClass('play');
+			    }
+			
+			    if (typeof (success) =='function') {
+			        success(touchSlider.cur)
+			    }
+			    if (prevIndex < 0) {
+			        prevIndex =  touchSlider.length-1;
+			        return false;
+			    }
+			}
+			//跳到下一页
+			function next() {
+			    if (touchSlider.cur < touchSlider.length) {
+			        touchSlider.cur++;
+			    }else {
+			        reset();
+			        return false;
+			    }
+			    var nextIndex = touchSlider.cur===1 ? touchSlider.length-1 : touchSlider.cur;
+			    
+			    var current=$('.on');
+			    var next=$('.page'+nextIndex);
+			    
+			    current.css(prefix.css+'Transform','translate3d(0,-'+v_h+'px,0)')
+					.removeClass('on');
+			    next.css(prefix.css+'Transform','translate3d(0,0,0)')
+			    	.addClass('on');
+			    
+			    if(utils.supports(prefix.lowercase+'Transition')){
+			        current.unbind()
+			        	.bind('webkitTransitionEnd otransitionend msTransitionEnd transitionend',function(e){
+			            if (!!$('.play')) {
+			                $('.play').removeClass('play');
+			            }
+			            $('.on').addClass('play');
+			        })
+			    }else{
+		            if (!!$('.play')) {
+		                $('.play').removeClass('play');
+		            }
+		            $('.on').addClass('play');
+			    }
+			
+			    if (typeof (success) =='function') {
+			        success(touchSlider.cur)
+			    }
+			}
+			//切换成功回调
+			function success(index){
+				// 第八屏取消更多职位动画
+				if(index == 8){
+					$('.finger').removeClass('flipOutX');
+					$('.page8-text1').removeClass('flipOutX');
+					$('.page8-text2').removeClass('flipInX');
+				}
+			}
+    	});
+	</script>
+</body>
+</html>
